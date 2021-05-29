c82f0d220e2bf350ef25ba19d60878e9497deca9
Ilia Mahjour-Shafiei <ilia.mahjour.shafiei@outlook.com>
2021-05-29 15:37:42 +0430

    Update vr.py

--- src/vr/vr.py
+++ src/vr/vr.py
@@ -5,9 +5,9 @@ def main():
 	rec = Recorder()
 	name = str(input("enter the file name without sufix: ")+".wav")
 	with rec.open(name, 'wb') as recfile:
-  		recfile1.start_recording()
+  		recfile.start_recording()
   		input("hit enter to exit")
-  		recfile1.stop_recording()
+        recfile.stop_recording()
 
 	system(f"ffmpeg -i {name} ~/{name}.mp3")
 	system(f"rm -rf {name}")
