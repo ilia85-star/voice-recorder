27a5c699c294fa1c2df448496e5ef59251ac1b8a
Ilia Mahjour-Shafiei <ilia.mahjour.shafiei@outlook.com>
2021-05-29 15:52:04 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+    'librec>=0.0.1',
+    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
