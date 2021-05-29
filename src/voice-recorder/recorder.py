1f6b24cb1d06664e5aa8dc5bb78ddc7b64a68e1a
Ilia Mahjour-Shafiei <ilia.mahjour.shafiei@outlook.com>
2021-05-29 14:45:57 +0430

    Update recorder.py

--- src/voice-recorder/recorder.py
+++ src/voice-recorder/recorder.py
@@ -1,12 +1,13 @@
 from librec import *
 from os import system
 
-rec = Recorder()
-name = str(input("enter the file name without sufix: ")+".wav")
-with rec.open(name, 'wb') as recfile2:
-    recfile2.start_recording()
-    input("hit enter to exit")
-    recfile2.stop_recording()
+def main():
+    rec = Recorder()
+    name = str(input("enter the file name without sufix: ")+".wav")
+    with rec.open(name, 'wb') as recfile2:
+        recfile2.start_recording()
+        input("hit enter to exit")
+        recfile2.stop_recording()
 
-system(f"ffmpeg -i {name} ~/{name}.mp3")
-system(f"rm -rf {name}")
+    system(f"ffmpeg -i {name} ~/{name}.mp3")
+    system(f"rm -rf {name}")
